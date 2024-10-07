530205eef5fbd5c277a9b03d588f9a75ed4f82d2
Nathachai_tn <119283041+Trainght@users.noreply.github.com>
2024-10-07 15:42:17 +0700

    Final jing jing React Problem 4

--- my-app/react-prob4/src/github_info.js
+++ my-app/react-prob4/src/github_info.js
@@ -1,6 +1,8 @@
 function Followers({numOfFollower , name,url}) {
     if (numOfFollower > 10000) {
-        return <a href={url} target="_blank" > {name}  {"("}  {numOfFollower} {"followers )"} </a>
+        // return <a href={url} target="_blank" > {name}  {"("}  {numOfFollower} {"followers)"} </a>
+        return <a href={url} target="_blank" >{`${name} (${numOfFollower} followers)`}</a>
+
     } else {
         return <a href={url} target="_blank" > {name} </a>
     }
