b54780ec1af80d7bfb58497bd2f73696c3cfa7a4
Nathachai_tn <119283041+Trainght@users.noreply.github.com>
2024-10-07 16:33:45 +0700

    Final React Problem 5

--- my-app/react-prob5/src/github_info.js
+++ my-app/react-prob5/src/github_info.js
@@ -5,6 +5,8 @@ export function GitHubInfo({UserInfo,size=200}, Name){
             src={UserInfo.imgURL}
             width={size}
             />
-        <a href={UserInfo.url} target="_blank" >{`${UserInfo.alt} (${UserInfo.followers} followers)`}</a>
+        {/* <a href={UserInfo.url} target="_blank" >{`${UserInfo.alt} (${UserInfo.followers} followers)`}</a> */}
+        <a href={UserInfo.url} target="_blank" >{UserInfo.alt}</a> <a>{`(${UserInfo.followers} followers)`}</a>
+
     </>
 }
